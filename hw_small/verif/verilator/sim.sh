aa1d53b98cad14f1235c4d00f336d41d39d6e530
Dian-Lun Lin <cyc4542000@gmail.com>
2021-10-21 22:58:32 -0600

    update

--- hw_small/verif/verilator/sim.sh
+++ hw_small/verif/verilator/sim.sh
@@ -4,7 +4,7 @@ filename="$1"
 files=()
 
 while read -r line; do
-  files+=("$line")
+  files+=("../../../verif/verilator/traces/$line")
 done < "$filename"
 #PATH=
 
@@ -15,25 +15,47 @@ trace_dir="traces"
 #file="sdp_pdp_32x16x32_pass_through_int8_0"
 #file="cdp_1x1x1_lrn3_int8_0"
 
-num_testbenches=$2
-num_threads=$3
-remains=${#files[*]}
-echo "==========Number of testbenches: "
-echo $remains
+LOOP_TESTBENCHES=(1 10 20 30 40 50 60 70 80 90 100)
+#LOOP_TESTBENCHES=(1)
+NUM_THREADS=$2
 
-while (($(($remains != 0)))); do
-  if (($(($remains < $num_threads))))
-  then
-    for (( i=$(($remains-1)); i>=0; i-- )); do
-      ./VNV_nvdla ${files[$i]}/trace.bin &
+cd ../../outdir/nv_small/verilator
+tbs=()
+TOTAL_TESTBENCHES=${#files[*]}
+
+for ltb in ${LOOP_TESTBENCHES[@]}; do
+  #echo $ltb
+  # duplicate testbenches
+  for file in ${files[@]}; do
+    for ((i=1;i<=$ltb;i++)); do
+      tbs+=($file)
     done
-    wait
-    remains=0
-  else
-    for (( i=$(($remains-1)); i>=$(($remains-$num_threads)); i-- )); do
-      ./VNV_nvdla ${files[$i]}/trace.bin &
-    done
-    wait
-    remains=$(($remains - $num_threads))
-  fi
+  done
+  
+  # shuffle testbenches
+  tbs=( $(shuf -e "${tbs[@]}") )
+
+
+  remains=${#tbs[*]}
+  printf "======================================= Number of testbenches: $remains =======================================" >> ~/RTLflow-benchmarks/hw_small/verif/verilator/res_time.out
+
+  time(
+  while (($(($remains != 0)))); do
+    if (($(($remains < $NUM_THREADS)))); then
+      for (( i=$(($remains-1)); i>=0; i-- )); do
+        ./VNV_nvdla ${files[$i]}/trace.bin &
+      done
+      wait
+      remains=0
+    else
+      for (( i=$(($remains-1)); i>=$(($remains-$NUM_THREADS)); i-- )); do
+        ./VNV_nvdla ${files[$i]}/trace.bin &
+      done
+      wait
+      remains=$(($remains - $NUM_THREADS))
+    fi
+  done
+  ) 2>> ~/RTLflow-benchmarks/hw_small/verif/verilator/res_time.out
+
+  tbs=()
 done
