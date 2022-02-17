862f9bb771dc3410682797b6a6f6f8114855024a
dian-lun-lin <cyc4542000@gmail.com>
2022-02-17 02:43:29 -0700

    pipeline

--- hw_small/verif/rtlflow/sim.sh
+++ hw_small/verif/rtlflow/sim.sh
@@ -1,7 +1,8 @@
 #CYCLES=(500 1000 5000 10000 50000 100000 500000 1000000)
 #NUM_TESTBENCHES=(64)
-CYCLES=($1)
-NUM_TESTBENCHES=($2)
+GPU_THREADS=$1
+CYCLES=($2)
+NUM_TESTBENCHES=($3)
 TIMES=1
 
 OUTPUT_DIR="../../../verif/rtlflow"
@@ -10,11 +11,12 @@ for ((k=1; k<=$TIMES; ++k)); do
   for nc in ${CYCLES[@]}; do
     for ntb in ${NUM_TESTBENCHES[@]}; do
 
-    make -j32 GPU_THREADS=$ntb
+    make -j32 GPU_THREADS=$GPU_THREADS
 
     cd ../../outdir/nv_small/
 
-    cd rtlflow_$ntb
+    cd rtlflow_$GPU_THREADS
+    printf "======================================= Number of GPU threads: $GPU_THREADS =======================================" >> $OUTPUT_DIR/c$nc\_tb$ntb.out
     printf "======================================= Number of testbenches: $ntb =======================================" >> $OUTPUT_DIR/c$nc\_tb$ntb.out
     printf "======================================= Cycles: $nc =======================================" >> $OUTPUT_DIR/c$nc\_tb$ntb.out
     (time ./VNV_nvdla $ntb $nc ../../../verif/verilator/tb_gen_scripts/random_traces/ ) 2>> $OUTPUT_DIR/c$nc\_tb$ntb.out
